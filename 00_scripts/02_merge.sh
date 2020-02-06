428eb7317af9bc88da1d5485ef4682ab5e64571f
Eric Normandeau <eric.normandeau.qc@gmail.com>
2020-02-06 09:07:41 -0500

    Changes to run on Asgaard

--- 00_scripts/02_merge.sh
+++ 00_scripts/02_merge.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-
-#SBATCH -D ./ 
-#SBATCH --job-name="merge"
-#SBATCH -o log-merge.out
-#SBATCH -c 1
-#SBATCH -p ibismini
-#SBATCH -A ibismini
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=type_your_mail@ulaval.ca
-#SBATCH --time=2-00:00
-#SBATCH --mem=10000
-
-<<<<<<< HEAD
-cd $SLURM_SUBMIT_DIR
-
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 SCRIPT=$0
 NAME=$(basename $0)
@@ -23,16 +8,6 @@ cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 INPUTFOLDER="03_trimmed"
 OUTPUTFOLDER="04_merged"
 
-	cat "$INPUTFOLDER"/*_R1.paired.fastq.gz >> "$OUTPUTFOLDER"/all_reads.left.fastq
- 
-	cat "$INPUTFOLDER"/*_R2.paired.fastq.gz >> "$OUTPUTFOLDER"/all_reads.right.fastq
-
-=======
-#move to present directory
-cd $(pwd)
-
 #cat all reads
-	cat "$INPUTFOLDER"/*_R1.paired.fastq.gz > "$OUTPUTFOLDER"/all_reads.left.fastq.gz
- 
-	cat "$INPUTFOLDER"/*_R2.paired.fastq.gz > "$OUTPUTFOLDER"/all_reads.right.fastq.gz
->>>>>>> 8629e02ffad09c4df82ffb278128308c999cbded
+gunzip -c "$INPUTFOLDER"/*_R1.paired.fastq.gz | pigz -c - > "$OUTPUTFOLDER"/all_reads.left.fastq.gz
+gunzip -c "$INPUTFOLDER"/*_R2.paired.fastq.gz | pigz -c - > "$OUTPUTFOLDER"/all_reads.right.fastq.gz
--- 00_scripts/03_assemble.sh
+++ 00_scripts/03_assemble.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-
-#SBATCH -D ./ 
-#SBATCH --job-name="assemble"
-#SBATCH -o log-assemble.out
-#SBATCH -c 8
-#SBATCH -p ibis2
-#SBATCH -A ibis2
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=type_your_mail@ulaval.ca
-#SBATCH --time=10-00:00
-#SBATCH --mem=220000
-
-cd $SLURM_SUBMIT_DIR
-
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 SCRIPT=$0
 NAME=$(basename $0)
@@ -20,15 +6,15 @@ LOG_FOLDER="98_log_files"
 cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 
 #Global variables
-READSLEFT="04_merged/all_left.fq"
-READSRIGHT="04_merged/all_right.fq"
+READSLEFT="04_merged/all_reads.left.fastq.gz"
+READSRIGHT="04_merged/all_reads.right.fastq.gz"
 #READSSINGLE="03_trimmed/*.trimmed.fastq.gz"
 
 #Trinity variables
 ##Required
 seqtype="--seqType fq"	     		# type of reads: ( fa, or fq )
 
-mem="--max_memory 200G"   		# suggested max memory to use by Trinity where limiting can be enabled. (jellyfish, sorting, etc)
+mem="--max_memory 150G"   		# suggested max memory to use by Trinity where limiting can be enabled. (jellyfish, sorting, etc)
                             		# provied in Gb of RAM, ie.  '--max_memory 10G'
 #paired reads:
 left="--left  $READSLEFT"    		#left reads, one or more file names (separated by commas, no spaces)
@@ -41,7 +27,7 @@ right="--right $READSRIGHT"    		#right reads, one or more file names (separated
                                   		# if paired: RF or FR,
                                    		#if single: F or R.   (dUTP method = RF)
                                    		#See web documentation.
-cpu="--CPU 8" 	                    		#number of CPUs to use, default: 2
+cpu="--CPU 12" 	                    		#number of CPUs to use, default: 2
 mincontiglength="--min_contig_length 200" 	#minimum assembled contig length to report
                                    		#(def=200)
 #corlongread="--long_reads <string>"	        #fasta file containing error-corrected or circular consensus (CCS) pac bio reads
@@ -66,37 +52,7 @@ output="--output 05_trinity_assembly_200/"              	#name of directory for 
                                     		#note: must include 'trinity' in the name as a safety precaution! )
 #cleanup="--full_cleanup"                  	#only retain the Trinity fasta file, rename as ${output_dir}.Trinity.fasta
 
-
-00_scripts/trinity_utils/Trinity $seqtype $mem $left $right $single \
+Trinity $seqtype $mem $left $right $single \
 	$strand $cpu $mincontiglength $corlongread \
 	$genomeguided $jaccard $normalize $notphase2 \
 	$output $cleanup 2>&1 | tee 98_log_files/"$TIMESTAMP"_trinityassembly.log
-
-
-
-#  --cite                          :show the Trinity literature citation
-#
-#  --verbose                       :provide additional job status info during the run.
-#
-#  --version                       :reports Trinity version (v2.1.1) and exits.
-#
-#  --show_full_usage_info          :show the many many more options available for running Trinity (expert usage).
-#
-#
-###############################################################################
-#
-#  *Note, a typical Trinity command might be:
-#
-#        Trinity --seqType fq --max_memory 50G --left reads_1.fq  --right reads_2.fq --CPU 6
-#
-#
-#    and for Genome-guided Trinity:
-#
-#        Trinity --genome_guided_bam rnaseq_alignments.csorted.bam --max_memory 50G
-#                --genome_guided_max_intron 10000 --CPU 6
-#
-#     see: /software6/apps/trinityrnaseq/2.1.1_gcc/sample_data/test_Trinity_Assembly/
-#          for sample data and 'runMe.sh' for example Trinity execution
-#
-#     For more details, visit: http://trinityrnaseq.github.io
-
